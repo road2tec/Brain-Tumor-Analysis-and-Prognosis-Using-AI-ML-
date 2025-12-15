f4c89271b40d25660ba85e274f355f6c5bef043f
Vishal Golhar <135511723+mrvishalg2004@users.noreply.github.com>
2025-12-15 23:11:56 +0530

    Added treatment plan

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings:
     SECRET_KEY: str = "your-secret-key-keep-it-secret"  # Change in production
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    MONGO_URI: str = "mongodb+srv://vishalroad2tech_db_user:vishal%40123@p1.qshu0ys.mongodb.net/campusconnect?retryWrites=true&w=majority"
+    MONGO_URI: str = os.getenv("MONGO_URI")
     DB_NAME: str = "campusconnect"
-    GEMINI_API_KEY: str = "AIzaSyCXvZUEE3ZjwEA57PayMNdduQ4Edl3o-G8"
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
 
 settings = Settings()
